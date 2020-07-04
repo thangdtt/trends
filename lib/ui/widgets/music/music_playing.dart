52d2fcfb62ed65cca3b17f26a73d2c907f347b6f
hai tho <haitho263@gmail.com>
2020-07-04 20:44:15 +0700

    add paly music

---
+++ lib/ui/widgets/music/music_playing.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:trends/data/models/music.dart';
+
+class MusicPlaying extends StatefulWidget {
+  const MusicPlaying({
+    Key key,
+    this.music,
+    this.playCallBack,
+    this.isPlaying,
+    this.nextCallBack,
+    this.previousCallBack,
+  }) : super(key: key);
+
+  @override
+  _MusicPlayingState createState() => _MusicPlayingState();
+  final Music music;
+  final bool isPlaying;
+  final Function() playCallBack;
+  final Function() nextCallBack;
+  final Function() previousCallBack;
+}
+
+class _MusicPlayingState extends State<MusicPlaying>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double aspectWidth = screenWidth / 360;
+    return Container(
+      height: 102*aspectWidth,
+      width: screenWidth,
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(300.0),
+            child: Container(
+              height: 50*aspectWidth,
+              width: 50*aspectWidth,
+              child: Align(
+                alignment: Alignment.center,
+                widthFactor: 0.4,
+                heightFactor: 1.0,
+                child: Image.network(widget.music.image),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 10 * aspectWidth,
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  widget.music.name,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontSize: 15 * screenWidth / 360,
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+                Text(
+                  widget.music.composer,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontSize: 15 * screenWidth / 360,
+                    fontWeight: FontWeight.normal,
+                    decoration: TextDecoration.none,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: 10 * aspectWidth,
+          ),
+          IconButton(
+            icon: Icon(Icons.skip_previous),
+            onPressed: () {
+              if (widget.previousCallBack != null) {
+                widget.previousCallBack();
+              }
+            },
+          ),
+          IconButton(
+            icon: Icon(widget.isPlaying ? Icons.play_arrow : Icons.stop),
+            onPressed: () {
+              if (widget.playCallBack != null) {
+                widget.playCallBack();
+              }
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.skip_next),
+            onPressed: () {
+              if (widget.nextCallBack != null) {
+                widget.nextCallBack();
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/widgets/music/music_tab.dart
+++ lib/ui/widgets/music/music_tab.dart
@@ -1,13 +1,17 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:trends/blocs/music/music_bloc.dart';
 import 'package:trends/data/models/music.dart';
+import 'package:trends/ui/widgets/music/music_playing.dart';
 import 'package:trends/ui/widgets/music/music_widget.dart';
 
 class MusicTab extends StatefulWidget {
-  const MusicTab({Key key,}) : super(key: key);
+  const MusicTab({
+    Key key,
+  }) : super(key: key);
 
   @override
   _MusicTabState createState() => _MusicTabState();
@@ -18,11 +22,31 @@ class _MusicTabState extends State<MusicTab>
   final RefreshController _refreshController =
       RefreshController(initialRefresh: false);
   List<Music> _currentMusics;
+  AudioPlayer audioPlayer = AudioPlayer();
+  int _currentIndex = 0;
+  Music _currentMusic;
+  bool isPlaying = false;
 
   @override
   void initState() {
     super.initState();
+
     BlocProvider.of<MusicBloc>(context).add(FetchMusics());
+    audioPlayer.onPlayerCompletion.listen((event) {
+      changeMusicIndex(_currentIndex + 1);
+    });
+  }
+
+  void changeMusicIndex(int index) {
+    _currentIndex = index;
+    if (_currentIndex > _currentMusics.length - 1) {
+      _currentIndex = 0;
+    } else if (_currentIndex < 0) {
+      _currentIndex = _currentMusics.length - 1;
+    }
+    setState(() {
+      _currentMusic = _currentMusics[_currentIndex];
+    });
   }
 
   @override
@@ -95,47 +119,79 @@ class _MusicTabState extends State<MusicTab>
   }
 
   Widget buildLoadedInput(List<Music> musics) {
-    return SmartRefresher(
-      enablePullDown: true,
-      enablePullUp: true,
-      header: ClassicHeader(),
-      footer: CustomFooter(
-        loadStyle: LoadStyle.ShowWhenLoading,
-        builder: (BuildContext context, LoadStatus mode) {
-          Widget body;
-          if (mode == LoadStatus.idle) {
-            body = SizedBox();
-          } else if (mode == LoadStatus.loading) {
-            body = CupertinoActivityIndicator();
-          } else if (mode == LoadStatus.failed) {
-            body = Text("Tải thêm không thành công !");
-          } else if (mode == LoadStatus.canLoading) {
-            body = Text("Buông để tải thêm");
-          } else {
-            body = Text("Không còn nội dụng để tải");
-          }
-          return Container(
-            height: 55.0,
-            child: Center(child: body),
-          );
-        },
-      ),
-      controller: _refreshController,
-      onRefresh: _onRefresh,
-      onLoading: _onLoading,
-      child: ListView.builder(
-        itemBuilder: (ctx, i) {
-          return MusicWidget(
-            music: musics[i],
-            callback: () {
-//              Navigator.of(context).pushNamed('', arguments: musics[i]);
-
-
+    return Stack(
+      children: <Widget>[
+        SmartRefresher(
+          enablePullDown: true,
+          enablePullUp: true,
+          header: ClassicHeader(),
+          footer: CustomFooter(
+            loadStyle: LoadStyle.ShowWhenLoading,
+            builder: (BuildContext context, LoadStatus mode) {
+              Widget body;
+              if (mode == LoadStatus.idle) {
+                body = SizedBox();
+              } else if (mode == LoadStatus.loading) {
+                body = CupertinoActivityIndicator();
+              } else if (mode == LoadStatus.failed) {
+                body = Text("Tải thêm không thành công !");
+              } else if (mode == LoadStatus.canLoading) {
+                body = Text("Buông để tải thêm");
+              } else {
+                body = Text("Không còn nội dụng để tải");
+              }
+              return Container(
+                height: 55.0,
+                child: Center(child: body),
+              );
             },
-          );
-        },
-        itemCount: musics.length,
-      ),
+          ),
+          controller: _refreshController,
+          onRefresh: _onRefresh,
+          onLoading: _onLoading,
+          child: ListView.builder(
+            itemBuilder: (ctx, i) {
+              return MusicWidget(
+                music: musics[i],
+                callback: () async {
+                  isPlaying = false;
+                  int result = await audioPlayer.play(musics[i].link);
+                  if (result == 1) {
+                    // success
+                    _currentMusic = _currentMusics[i];
+                    isPlaying = true;
+                    setState(() {});
+                  }
+//              Navigator.of(context).pushNamed('', arguments: musics[i]);
+                },
+              );
+            },
+            itemCount: musics.length,
+          ),
+        ),
+        if (_currentMusic != null) Positioned(
+          bottom: 0,
+          child: MusicPlaying(
+                  isPlaying: isPlaying,
+                  music: _currentMusic,
+                  nextCallBack: () {
+                    changeMusicIndex(_currentIndex + 1);
+                  },
+                  playCallBack: () {
+                    isPlaying = !isPlaying;
+                    if (isPlaying) {
+                      audioPlayer.resume();
+                    } else {
+                      audioPlayer.pause();
+                    }
+                    setState(() {});
+                  },
+                  previousCallBack: () {
+                    changeMusicIndex(_currentIndex + 1);
+                  },
+                ),
+        ) else const SizedBox()
+      ],
     );
   }
 
